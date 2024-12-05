757f781976b71b7f20d865ae94255cccd6d92cb3
Mi-Hao <kikimmora79@gmail.com>
2024-12-05 13:59:16 +0300

    add main and tester

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+/**
+ * Главный класс для запуска тестов производительности ArrayList и LinkedList.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        tester tester = new tester(100000); // Тестирование на 100000 операций
+
+        // Тестируем ArrayList
+        System.out.println("ArrayList:");
+        tester.testArrayList();
+
+        // Тестируем LinkedList
+        System.out.println("LinkedList:");
+        tester.testLinkedList();
     }
 }
--- src/main/java/org/example/tester.java
+++ src/main/java/org/example/tester.java
@@ -1,4 +1,84 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Класс для тестирования производительности ArrayList и LinkedList.
+ */
 public class tester {
+
+    private final int testSize;
+
+    /**
+     * Конструктор класса tester.
+     *
+     * @param testSize Количество операций для тестирования.
+     */
+    public tester(int testSize) {
+        this.testSize = testSize;
+    }
+
+    /**
+     * Метод для тестирования ArrayList.
+     */
+    public void testArrayList() {
+        List<Integer> arrayList = new ArrayList<>();
+        runTests(arrayList, "ArrayList");
+    }
+
+    /**
+     * Метод для тестирования LinkedList.
+     */
+    public void testLinkedList() {
+        List<Integer> linkedList = new LinkedList<>();
+        runTests(linkedList, "LinkedList");
+    }
+
+    /**
+     * Метод для выполнения тестов производительности.
+     *
+     * @param list    Тестируемый список (ArrayList или LinkedList).
+     * @param listType Тип списка для вывода в консоль.
+     */
+    private void runTests(List<Integer> list, String listType) {
+        System.out.println("Testing " + listType + " with " + testSize + " operations.");
+
+        long addTime = measureTime(() -> {
+            for (int i = 0; i < testSize; i++) {
+                list.add(i);
+            }
+        });
+
+        long getTime = measureTime(() -> {
+            for (int i = 0; i < testSize; i++) {
+                list.get(i);
+            }
+        });
+
+        long removeTime = measureTime(() -> {
+            for (int i = testSize - 1; i >= 0; i--) {
+                list.remove(i);
+            }
+        });
+
+        System.out.printf("%-10s | %-10s | %-10s | %-10s%n", "Method", "Operations", "Time (ms)", "List Type");
+        System.out.printf("%-10s | %-10d | %-10d | %-10s%n", "Add", testSize, addTime, listType);
+        System.out.printf("%-10s | %-10d | %-10d | %-10s%n", "Get", testSize, getTime, listType);
+        System.out.printf("%-10s | %-10d | %-10d | %-10s%n", "Remove", testSize, removeTime, listType);
+        System.out.println();
+    }
+
+    /**
+     * Метод для измерения времени выполнения кода.
+     *
+     * @param runnable Лямбда или анонимный класс для выполнения кода.
+     * @return Время выполнения в миллисекундах.
+     */
+    private long measureTime(Runnable runnable) {
+        long startTime = System.currentTimeMillis();
+        runnable.run();
+        return System.currentTimeMillis() - startTime;
+    }
 }
